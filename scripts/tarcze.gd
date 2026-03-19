c08f998996a27213237d4ac4a91307e937d98a86
WaleBa <wiktor@danbes.com.pl>
2026-03-19 19:02:46 +0100

    smooth tarcze

--- scripts/tarcze.gd
+++ scripts/tarcze.gd
@@ -10,5 +10,7 @@ func _process(delta):
 	var viewport_center = Vector2(get_viewport().get_visible_rect().size.x /2, get_viewport().get_visible_rect().size.y /2)
 	var mouse_pos = get_viewport().get_mouse_position()
 	var dir = mouse_pos - viewport_center
-	look_at(Vector3(position.x + dir.x * 100000000, 0, position.z + dir.y * 100000000), Vector3.UP)
-	
+	var direction = Vector3(position.x + dir.x * 100000000, 0, position.z + dir.y * 100000000)
+	#look_at(Vector3(position.x + dir.x * 100000000, 0, position.z + dir.y * 100000000), Vector3.UP)
+	var target_basis = Basis().looking_at(direction, Vector3.UP)
+	global_transform.basis = global_transform.basis.slerp(target_basis, delta * 2)
